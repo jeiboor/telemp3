d624e257b4c3bfe067911fd124eebbd0bc20012d
MR_saf <jeiboor@users.noreply.github.com>
2016-07-24 12:09:15 +0430

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+api_key = '' 
+# load run.sh
+owner {
+    187161700
+}
+    admins {
+        187161700
+        160890821
+}
+load(reload)
+load:
+    load {
+        plugins
+        run.sh
+        readmd.md 
+        feedback.py 
+        status.py 
+    }
+    
+            doc = "T property."
+            def fget(self):
+                return sel
+            def fset(self, value):
+                sel = value
