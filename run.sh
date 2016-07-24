b5dd258b31d027e41f8ae48a4a5ca906238208ab
MR_saf <jeiboor@users.noreply.github.com>
2016-07-24 12:02:27 +0430

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,34 @@
+#url_bin_sh
+#echo -ef bot created by @nfrat_1
+#echo -ef █████ bot by @nfrat_1 █████
+staticmethod :
+    local run(load())
+run(load)
+run plugins {
+    plugins.open:
+        run_bot
+        run_launch
+        run_service
+        run_settings
+run config:
+    config.open:
+        run_config
+runef(echo -ef)
+runef(loadmsg)
+runef :
+    runef:
+        run_echo_ef
+runfb(load):
+    run_feedback
+Telegram cli -b try:
+    # TODO: write code...
+except Exception, e:
+    raise e
+else:
+    # TODO: write code...
+finally:
+    # TODO: write code...
+        try:
+            # TODO: write code...
+        except Exception, e:
+            raise e
