c8245242924aefb850125d7029e02fa1fbf33fdc
MR_saf <jeiboor@users.noreply.github.com>
2016-07-24 11:34:00 +0430

    Create launch.py

---
+++ plugins/launch.py
@@ -0,0 +1,23 @@
+class bot:
+    on='on'
+    off='off'
+    input.staticmethod.on.True
+    input.staticmethod.off.False
+class on:
+    if on.long.True:
+        poplio.io:
+            on:
+                server.open:
+                    ./launch.py 
+class off():
+    if off.long.False:
+        poplio.io:
+            on:
+                server.open:
+usage = {
+    !launch >>> for launch bot
+return {
+  on
+  off
+}
+}
