3a1d476ea17e4ee0c08570cdb589e00c5c5c9035
MR_saf <jeiboor@users.noreply.github.com>
2016-07-24 11:25:17 +0430

    Add files via upload

---
+++ status.py
@@ -0,0 +1,19 @@
+{
+status.number=some.from.number
+from dy
+  {
+    text:member= for moderators only 
+    text:text= function run (msg, matches)
+  return={
+    useage={status number 
+      patterns={
+        ^(title)(settings)$
+        @echo off={you're admin
+      {
+    {
+  {
+{
+  {
+    end 
+      end
+{
