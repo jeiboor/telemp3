f0198d2020f469272acd07b8edddcae71c6d0615
MR_saf <jeiboor@users.noreply.github.com>
2016-07-24 11:30:35 +0430

    Create bot.py

---
+++ plugins/bot.py
@@ -0,0 +1,20 @@
+/on='on'
+/off='off'
+if sudo /on:
+    local url=https://ide.c9.io/jeiboor/sf 
+poplio=io:
+    ./cd self:
+        ./launch.sh
+        ./autolaunch.sh
+    (read all*)
+if /on='on':
+    on.poplio=io 
+    print('bot is on')
+if /on='off':
+    print('')
+if /off='off':
+    off.poplio=io.false
+    print('bot is off')
+if /off='on':
+    print('')
+    
